2f20dfb046420ffe6846327b6c9f7adb224a3b62
kisaka541 <kisakajael4@gmail.com>
2025-05-15 17:25:00 +0300

    initial commit

---
+++ intro_class.py
@@ -0,0 +1,18 @@
+print('Jael Kisaka')
+print(100)
+print(1000)
+
+x=20
+y=5
+z=x+y
+print(z)
+
+#calculate area of a square
+side=20
+area=side*side
+print(area)
+#calculate area of  a rectangle
+length=20
+width=10
+area=length*width
+print(area)
