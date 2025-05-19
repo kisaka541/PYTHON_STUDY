97e7cdd6a019127fadd3e31b761a74870520b2e5
kisaka541 <kisakajael4@gmail.com>
2025-05-19 15:07:14 +0300

    PYTHON_STUDY

---
+++ areaofcircle.py
@@ -0,0 +1,5 @@
+#area of a circle
+pie=3.142
+r=20
+area=pie*r*r
+print(area)
---
+++ strings.py
@@ -0,0 +1,32 @@
+first_name="Jael"
+second_name="Kisaka"
+x=1000
+y=99.91
+z=False
+
+print(type(first_name))
+print(type(x))
+print(type(y))
+print(type(z))
+
+first_name="Jael"
+second_name="Kisaka"
+full_name=first_name+' '+second_name
+print(full_name)
+
+x='1000'
+y='2000'
+z=x+y
+print(z)
+#strings concut ,they dont addup
+
+text="Python programming"
+print(text[10])
+print(text[7:18])
+print(text[0:6])
+
+
+#string methods
+my_text="HELlo"
+my_text=my_text.capitalize()
+print(my_text)
